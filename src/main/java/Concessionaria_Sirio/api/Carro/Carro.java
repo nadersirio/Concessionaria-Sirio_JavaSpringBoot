99b6f74e92929be0193e9f7335a86971df53bd47
Náder W. S. Sírio <nadersirio@gmail.com>
2025-02-24 21:59:06 -0300

    Implement Carro Repository and Constructor to save on Database

--- src/main/java/Concessionaria_Sirio/api/Carro/Carro.java
+++ src/main/java/Concessionaria_Sirio/api/Carro/Carro.java
@@ -1,7 +1,36 @@
 package Concessionaria_Sirio.api.Carro;
 
-
 import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record Carro(String marca, String modelo, String ano, BigDecimal TabelaFIPE, String placa, DataRevisoes revisoes) {
+@Table(name = "Carro")
+@Entity(name = "Carro")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Carro {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String modelo;
+  private String marca;
+  private String ano;
+  private BigDecimal tabelaFipe;
+
+  public Carro(DadosCadastroCarro dados) {
+    this.modelo = dados.modelo();
+    this.marca = dados.marca();
+    this.ano = dados.ano();
+    this.tabelaFipe = dados.TabelaFIPE();
+  }
 }
