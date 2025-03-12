2d71393b77c374bed8021736c417bc3f77b9737f
Náder W. S. Sírio <nadersirio@gmail.com>
2025-03-11 22:12:53 -0300

    add marca archives to insert carro info on the database

--- src/main/java/Concessionaria_Sirio/api/Carro/Carro.java
+++ src/main/java/Concessionaria_Sirio/api/Carro/Carro.java
@@ -1,10 +1,14 @@
 package Concessionaria_Sirio.api.Carro;
 
 import java.math.BigDecimal;
+
+import Concessionaria_Sirio.api.Marca.Marca;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -23,14 +27,21 @@ public class Carro {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String modelo;
-  private String marca;
   private String ano;
   private BigDecimal tabelaFipe;
 
+  @ManyToOne
+  @JoinColumn(name = "marca", referencedColumnName = "id")
+  private Marca marca;
+
   public Carro(DadosCadastroCarro dados) {
     this.modelo = dados.modelo();
-    this.marca = dados.marca();
     this.ano = dados.ano();
     this.tabelaFipe = dados.TabelaFIPE();
   }
+
+  public void setMarca(Marca marca) {
+    this.marca = marca;
+  }
+
 }
--- src/main/java/Concessionaria_Sirio/api/controler/CadastroCarro.java
+++ src/main/java/Concessionaria_Sirio/api/controler/CadastroCarro.java
@@ -3,6 +3,10 @@ package Concessionaria_Sirio.api.controler;
 import Concessionaria_Sirio.api.Carro.Carro;
 import Concessionaria_Sirio.api.Carro.CarroRepository;
 import Concessionaria_Sirio.api.Carro.DadosCadastroCarro;
+import Concessionaria_Sirio.api.Marca.DadosMarca;
+import Concessionaria_Sirio.api.Marca.Marca;
+import Concessionaria_Sirio.api.Marca.MarcaRepository;
+import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +21,18 @@ public class CadastroCarro {
     @Autowired 
     private CarroRepository repository;
 
+    @Autowired
+    private MarcaRepository marcaRepository;
+
     @PostMapping
+    @Transactional
     public void Cadastro(@RequestBody DadosCadastroCarro dados) {
-        repository.save(new Carro(dados));
+
+        DadosMarca dadosMarca = dados.marca();
+        Marca marcasalva = marcaRepository.save(new Marca(dadosMarca));
+
+        Carro carro = new Carro(dados);
+        carro.setMarca(marcasalva);
+        repository.save(carro);
     }
 }
